08058bda7d6fee08ef7bffbdc011bb2893aec560
Angie <angiewashburn1997@gmail.com>
2016-05-06 16:14:59 -0400

    Uppercase Lowercase

---
+++ StringsUpperLower/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsUpperLower
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userNameUpper = "WASH";
+            string userNameLower = "wash";
+
+            Console.WriteLine("Your name in upper case is {0}.  Your name in lower case is {1}.", userNameLower, userNameUpper);
+        }
+    }
+}

aae9aa7e70235320c7fa9910595e90141c49dfdd
Angie <angiewashburn1997@gmail.com>
2016-05-06 16:07:57 -0400

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string noteToSelf = "Your journey in coding is just beginning.";
+            {
+
+                Console.WriteLine("Dear Angie, remember: {0}!", noteToSelf);
+                Console.Read();
+            }
+        }
+        
+    }
+}
---
+++ StringsStudentChallenge/Strings_CakeEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringsStudentChallenge
+{
+    public class Program
+        {
+            static void Main()
+        { 
+            string ingredientOne = "flour";
+            string ingredientTwo = "sugar";
+            string ingredientThree = "butter";
+            string ingredientFour = "milk";
+            string ingredientFive = "eggs";
+
+               
+            Console.WriteLine("We need {0}, {1}, {2}, {3}, and {4} for our cake!", ingredientOne, ingredientTwo, ingredientThree, ingredientFour, ingredientFive);
+            Console.Read();
+        }
+    }
+}
+    
+
+    
+   

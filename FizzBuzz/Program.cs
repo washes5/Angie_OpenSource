5338cc510ad498fb3101fac97b8a4521336a98c6
Angie <angiewashburn1997@gmail.com>
2016-05-06 16:57:33 -0400

    Fizz Buzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (var i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                    Console.Read();
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    Console.Read();
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    Console.Read();
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                    Console.Read();
+
+                }
+            }
+        }
+    }
+}
---
+++ Integers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integers
+{
+    class Program
+    {
+        static void Main()
+        { 
+
+        public float bodyTemp = 98.6f;
+        public int tempOutside = 70;
+            
+        
+        Console.WriteLine (bodyTemp);
+        Console.Read();
+
+        }
+    }
+}

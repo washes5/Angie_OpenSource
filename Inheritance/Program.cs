03695cca46eed348292382d1fd732d820c31807b
Angie <angiewashburn1997@gmail.com>
2016-05-16 14:27:36 -0400

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Athlete connor = new Athlete();
+        }
+
+    }
+
+    public class Athlete
+    {
+        public string FirstName { get; set; }
+    }
+}
+
+
+   /* public class Athlete
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+
+        public int age { get; set; }
+
+        public bool highSchool { get; set; }
+        public bool college { get; set; }
+    }
+    class Basketball : Athlete
+    {
+        public int jerseyNumber { get; set; }
+        public int yearsPlayed { get; set; }
+        public int averagePointsGame { get; set; }
+
+        public string nickName { get; set; }
+
+        public bool tallerThanSixFoot { get; set; }
+
+        public void introduction()
+        {
+            Console.Write("Introducing {0}", jerseyNumber);
+            Console.Read();
+        }
+    }
+    class Lacrosse : Athlete
+    {
+        public int numberPenalities { get; set; }
+
+        public bool mouthGuard { get; set; }
+    }
+}
+*/
+
+        
+
+
+
+
+
+    
---
+++ Inheritance_Athlete/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Athlete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Athlete connor = new Athlete();
+            connor.lastName = "Washburn";
+
+            Athlete noah = new Athlete();
+            noah.lastName = "Washburn";
+
+            Athlete luke = new Athlete();
+            luke.lastName = "Washburn";
+
+            Athlete brevin = new Athlete();
+            brevin.lastName = "Barker";
+
+        }
+    }
+
+    public class Athlete
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string nameOfSport { get; set; }
+
+        public int age { get; set; }
+
+        public bool siblings { get; set; }
+
+        public void playerName()
+        {
+            Console.WriteLine("My name is {0}", lastName);
+            Console.ReadLine();
+        }
+    }
+
+
+
+    public class lacrosse : Athlete
+    {
+        public int jerseyNumber { get; set; }
+
+        public bool mouthGuard { get; set; }
+
+        lacrosse noah = new lacrosse();
+
+
+        public void series()
+        {
+            Console.WriteLine("Athlete bio:", lastName, jerseyNumber);
+            Console.Read();
+        }
+        
+    }
+
+    
+    
+
+}
